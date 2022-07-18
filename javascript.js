962820060aaf853300384a1015c279763a97b42b
Bryan <bryanhumb.marroquin@gmail.com>
2022-07-17 20:49:46 -0700

    Added game logic and game results

--- javascript.js
+++ javascript.js
@@ -1,17 +1,65 @@
-const computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
 
-function playerSelection() {
-    const playerSelection = prompt("Type your choice: ");
+function game(){
+    for (let i = 0; i < 5; i++) {
+        console.log("-----------This is a new round-----------");
+        playRound(getPlayerChoice(), getComputerChoice());
 }
+console.log ("-----------Results-----------");
+if (playerScore > computerScore) {
+    console.log("You won the game against the computer!");
+} else if (playerScore < computerScore) {
+    console.log("A computer beat you lol");
+} else {
+    console.log("The game ends in a tie!");
+}
+}
+
+function playRound(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            console.log("It's a tie!");
+        } else if (playerSelection === "rock" && computerSelection === "paper") {
+            console.log("You lost this time.");
+            computerScore++;
+        } else if (playerSelection === "rock" && computerSelection === "scissors") {
+            console.log("You won this time!");
+            playerScore++;
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            console.log("You won this time!");
+            playerScore++;
+        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            console.log("You lost this time.");
+            computerScore++;
+        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            console.log("You lost this time.");
+            computerScore++;
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            console.log("You won this time!");
+            playerScore++;
+        } else {
+            console.log("Invalid input");
+        }
+    }
+
+function getPlayerChoice() {
+    let getPlayerSelection = prompt("Type your choice: ");
+    getPlayerSelection = getPlayerSelection.toLowerCase();
+    console.log("Your chose: " + getPlayerSelection)
+    return getPlayerSelection;
+}
+// Pick a random number from 1 to 3. 1 being "Rock", 2 being "Paper", 3 being "Scissors". //
 
 function getComputerChoice() {
-    getChoice = Math.floor(Math.random()*3) + 1;
+    let getChoice = Math.floor(Math.random()*3) + 1;
+    let selection;
     if (getChoice === 1) {
-        getChoice = "Rock";
+        selection = "rock";
     } else if (getChoice === 2) {
-        getChoice = "Paper";
+        selection = "paper";
     } else {
-        getChoice = "Scissors";
+        selection = "scissors";
 }
-
+    console.log("Computer chose: " + selection);
+    return selection;
 }

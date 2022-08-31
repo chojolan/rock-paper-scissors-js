ec795d381589266156b8d41674a655efb1a591b9
Bryan <bryanhumb.marroquin@gmail.com>
2022-08-30 20:30:35 -0700

    Replaced console.log code with textContent

--- javascript.js
+++ javascript.js
@@ -2,78 +2,23 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => { button.addEventListener('click', getPlayerChoice)});
 
 const results = document.querySelector(".results");
+const roundResults = document.querySelector(".roundResults");
 
 let playerScore = 0;
 let computerScore = 0;
 
+const player = document.querySelector('.playerPoints');
+player.textContent = `Player's score: ${playerScore}`;
+
+const computer = document.querySelector(".computerPoints");
+computer.textContent = `Computer's score: ${computerScore}`;
+
 function game(){
 // Play the game for 5 rounds, each round call the playRound function to get both inputs //
-for (let i = 0; i < 5; i++) {
-    console.log("-----------Starting new round-----------");
-    playRound(getPlayerChoice(), getComputerChoice());
-}
-// Print the results //
-
-console.log ("-----------Results-----------");
-if (playerScore > computerScore) {
-    console.log("You won the game against the computer!");
-} else if (playerScore < computerScore) {
-    console.log("A computer beat you lol");
-} else {
-    console.log("The game ends in a tie!");
-}
+while (playerScore < 5 || computerScore < 5) {
+    playRound();
 }
 
-// Compare both inputs and update each player's scores //
-
-function playRound(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {
-            console.log("It's a tie!");
-            console.log("Your score: " + playerScore);
-            console.log("Computer's score: " + computerScore);
-        } else if (playerSelection === "rock" && computerSelection === "paper") {
-            console.log("You lost this time.");
-            computerScore++;
-            console.log("Your score: " + playerScore);
-            console.log("Computer's score: " + computerScore);
-        } else if (playerSelection === "rock" && computerSelection === "scissors") {
-            console.log("You won this time!");
-            playerScore++;
-            console.log("Your score: " + playerScore);
-            console.log("Computer's score: " + computerScore);
-        } else if (playerSelection === "paper" && computerSelection === "rock") {
-            console.log("You won this time!");
-            playerScore++;
-            console.log("Your score: " + playerScore);
-            console.log("Computer's score: " + computerScore);
-        } else if (playerSelection === "paper" && computerSelection === "scissors") {
-            console.log("You lost this time.");
-            computerScore++;
-            console.log("Your score: " + playerScore);
-            console.log("Computer's score: " + computerScore);
-        } else if (playerSelection === "scissors" && computerSelection === "rock") {
-            console.log("You lost this time.");
-            computerScore++;
-            console.log("Your score: " + playerScore);
-            console.log("Computer's score: " + computerScore);
-        } else if (playerSelection === "scissors" && computerSelection === "paper") {
-            console.log("You won this time!");
-            playerScore++;
-            console.log("Your score: " + playerScore);
-            console.log("Computer's score: " + computerScore);
-        } else {
-            console.log("Invalid input");
-        }
-    }
-
-// Ask user for their choice and conver the string to lowercase to avoid errors when comparing an uppercase string to a lowercase string//
-
-function getPlayerChoice() {
-    let getPlayerSelection = prompt("Type your choice: ");
-    getPlayerSelection = getPlayerSelection.toLowerCase();
-    console.log("You chose: " + getPlayerSelection)
-    return getPlayerSelection;
-}
 // Pick a random number from 1 to 3. 1 being "Rock", 2 being "Paper", 3 being "Scissors". //
 
 function getComputerChoice() {
@@ -86,6 +31,44 @@ function getComputerChoice() {
     } else {
         selection = "scissors";
 }
-    console.log("Computer chose: " + selection);
     return selection;
+}
+
+// Print the results //
+
+if (playerScore === 5) {
+    results.textContent = "You won the game against the computer!";
+} else if (computerScore === 5) {
+    results.textContent = "A computer beat you lol";
+} else {
+    results.textContent = "The game ends in a tie!";
+}
+}
+
+// Compare both inputs and update each player's scores //
+
+function playRound(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            roundResults.textContent = `It's a tie!`;
+            playerPoints.textContent = `Your score: ${playerScore}`;
+            computerPoints.textContent = `Computer's score: $computerScore`;
+        } else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock") {
+            roundResults.textContent = `You lost this time.`;
+            computerScore++;
+            roundResults.textContent = `Your score: ${playerScore}`;
+            roundResults.textContent = `Computer's score: ${computerScore}`;
+        } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
+            roundResults.textContent = `You won this time!`;
+            playerScore++;
+            roundResults.textContent = `Your score: ${playerScore}`;
+            roundResults.textContent = `Computer's score: ${computerScore}`;
+        } else {
+            roundResults.textContent = `Invalid input`;
+        }
+    }
+
+// Ask user for their choice and conver the string to lowercase to avoid errors when comparing an uppercase string to a lowercase string//
+
+function getPlayerChoice() {
+
 }
